5cdf88a511e33261cddd79ea7dbfcd325dd93a90
Mitchel B <staysaucy555@gmail.com>
2022-10-17 20:18:09 -0400

    added var special and numbers

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ function generatePassword() {
 
   var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var specials = "!@#$%^&*()-_=+[{]};:,<.>/?";
+  var numbers = "0123456789";
 }
 // Write password to the #password input
 function writePassword() {
