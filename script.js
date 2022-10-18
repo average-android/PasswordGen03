4d7ff8a3503da1c473faa4ec18001938259711f4
Mitchel B <staysaucy555@gmail.com>
2022-10-17 21:15:02 -0400

    Added several confirm functions

--- script.js
+++ script.js
@@ -8,6 +8,19 @@ function generatePassword() {
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
   var specials = "!@#$%^&*()-_=+[{]};:,<.>/?";
   var numbers = "0123456789";
+  var passwordStart= "";
+
+  var uppercaseConfirm = confirm("Press ok if you want Uppercase characters in your password.");
+
+  if (uppercaseConfirm) {passwordStart += uppercase}
+
+  var lowercaseConfirm = confirm("Press ok if you want Lowercase characters in your password.");
+
+  if (lowercaseConfirm) {passwordStart += lowercase}
+
+  var specialsConfirm = confirm("Press ok if you want special characters in your password.");
+
+  if (specialsConfirm) {passwordStart += specials}
 }
 // Write password to the #password input
 function writePassword() {
